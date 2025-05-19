b06a2ef032f2f8cee858dc503fbe04e37bef33be
Gutts-n <57202549+Gutts-n@users.noreply.github.com>
2025-05-19 13:18:07 -0300

    [md] Updated the code to apply to package_search and added logger to show the error when it throws error

--- ckanext/zurich/logic/action.py
+++ ckanext/zurich/logic/action.py
@@ -1,13 +1,15 @@
 import ckan.plugins.toolkit as tk
 import ckan.lib.uploader as uploader
 import os 
+from logging import getLogger
 
-@tk.side_effect_free
-@tk.chained_action
-def package_show(up_func, context, data_dict):
-    package = up_func(context, data_dict)
+log = getLogger(__name__)
 
-    for resource in package['resources'] or []:
+def add_resource_download_uri(package):
+    for resource in package.get('resources', []):
+        if resource['url_type'] != 'upload':
+            resource['download_url'] = resource['url']
+            continue
         upload = uploader.get_resource_uploader(resource)
         filename = os.path.basename(resource['url'])
         key_path = upload.get_path(resource['id'], filename)
@@ -15,6 +17,28 @@ def package_show(up_func, context, data_dict):
                     'ResponseContentDisposition':
                         'attachment; filename=' + filename,
                 }
-        resource['download_url'] = upload.get_signed_url_to_key(key_path, params, read_only=True)
+        try:
+            resource['download_url'] = upload.get_signed_url_to_key(key_path, params, read_only=True)
+        except Exception as e:
+            log.error(f"An error occurred trying to get the {resource.get('name')} resource download URI")
+            log.error(e)
+            resource['download_url'] = resource['url']
+
+@tk.side_effect_free
+@tk.chained_action
+def package_show(up_func, context, data_dict):
+    package = up_func(context, data_dict)
+
+    add_resource_download_uri(package)
 
     return package
+    
+@tk.side_effect_free
+@tk.chained_action
+def package_search(up_func, context, data_dict):
+    result = up_func(context, data_dict)
+
+    for package in result.get("results", []):
+        add_resource_download_uri(package)
+
+    return result
