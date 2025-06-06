43ebfc45faa16429aac9defe9a3ad673e2bb1098
Leonardo Yuri Farias <57202549+Gutts-n@users.noreply.github.com>
2025-06-06 18:54:17 -0300

    Updated code to use proxied downloads

--- ckanext/zurich/logic/action.py
+++ ckanext/zurich/logic/action.py
@@ -10,19 +10,7 @@ def add_resource_download_uri(package):
         if resource['url_type'] != 'upload':
             resource['download_url'] = resource['url']
             continue
-        upload = uploader.get_resource_uploader(resource)
-        filename = os.path.basename(resource['url']).lower()
-        key_path = upload.get_path(resource['id'], filename)
-        params = {
-                    'ResponseContentDisposition':
-                        'attachment; filename=' + filename,
-                }
-        try:
-            resource['download_url'] = upload.get_signed_url_to_key(key_path.lower(), params, read_only=True, expires_in=tk.config.get('ckanext.zurich.resource_download_url_expiration_time', 3600))
-        except Exception as e:
-            log.error(f"An error occurred trying to get the {resource.get('name')} resource download URI")
-            log.error(e)
-            resource['download_url'] = resource['url']
+        resource['download_url'] = f"{resource['url']}?proxied_download=true"
 
 @tk.side_effect_free
 @tk.chained_action
